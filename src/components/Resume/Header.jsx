5ac5243bfb9ed04a1580b6b2227b649a6cfc0d97
Nate Tanner <40620787+tannernd@users.noreply.github.com>
2023-11-05 00:28:23 -0600

    Small Resume Header Update for email and phone links

--- src/components/Resume/Header.jsx
+++ src/components/Resume/Header.jsx
@@ -3,7 +3,7 @@ function ResumeHeader({header}) {
         <div>
             <h1 className="text-center">{header.name}</h1>
             <h4 className="text-center">{header.title}</h4>
-            <p className="text-center">Phone: {header.phone} | Email: {header.email} | {header.location}</p>
+            <p className="text-center">Phone: <a href={`tel:${header.phone}`}>{header.phone}</a> | Email: <a href={`mailto:${header.email}`}>{header.email}</a> | {header.location}</p>
             <h3 className="text-center">Professional Summary</h3>
                 <p>{header.summary}</p>
                 <h3 className="text-center">Technical Skills</h3>
