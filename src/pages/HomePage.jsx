c900d30b4d75c715950c0cfcd05e9a42440f05d5
Nate Tanner <40620787+tannernd@users.noreply.github.com>
2023-11-05 01:33:42 -0700

    Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,22 @@
 export default function HomePage() {
     return (
-        <div>Home</div>
+        <div className="container">
+            <h1 className="text-center">Welcome to My Portfolio Site</h1>
+            <p>Are you in search of a skilled and creative web developer to bring your digital projects to life? Look no further! I am here to showcase my expertise and passion for web development through a collection of projects that highlight my skills, experience, and dedication to crafting stunning and functional websites.</p>
+            <h3>My Portfolio</h3>
+            <p>Explore my portfolio to see a range of projects I've completed. From responsive and user-friendly websites to complex web applications, my work showcases the diversity of my skills. Whether you need a visually appealing e-commerce site, a secure online platform, or a sleek, modern blog, I've got you covered.</p>
+            <h3>What I Can Offer</h3>
+            <ul>
+                <li><span className="fw-bold">E-commerce Solutions:</span> Looking to start or upgrade your online store? I can help you develop robust e-commerce websites with secure payment gateways, intuitive navigation, and sleek product displays.</li>
+                <li><span className="fw-bold">Web App Development:</span> If you have a complex idea for a web application, I'm up for the challenge. I can turn your vision into a fully functional web app that users will love.</li>
+                <li><span className="fw-bold">Custom Web Development:</span> I specialize in creating tailor-made websites that meet your unique needs. I'll work closely with you to ensure your site reflects your brand and vision.</li>
+                <li><span className="fw-bold">Mobile Responsiveness:</span> In today's mobile-driven world, having a responsive design is crucial. All of my projects are optimized for a seamless user experience on any device.</li>
+                <li><span className="fw-bold">SEO Optimization:</span> Your website should be easily discoverable. I can implement SEO best practices to improve your site's visibility and rankings in search engines.</li>
+            </ul>
+            <h3>Let's Collaborate</h3>
+            <p>Ready to take your web presence to the next level? Let's discuss your project and explore how we can work together. I'm always excited to take on new challenges, and I thrive on turning your ideas into reality. Get in touch with me today, and let's create something extraordinary!</p>
+
+            <p>Thank you for visiting my portfolio. I look forward to the opportunity to bring your web development dreams to life.</p>
+        </div>
     )
 }
